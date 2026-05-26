2be20bd60fc03ce8bbb179261e8b754eedac8301
vivek redekar <redekarvivek126@gmail.com>
2026-05-26 17:10:30 +0530

    empty repo.

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("hello Devops Team!!!");
-console.log("Learning CI/CD with github actions!!!");
--- test.js
+++
@@ -1,5 +0,0 @@
-console.log("Starting tests...");
-setTimeout(() => {
-   console.log("waiting 3 seconds")  
-}, 3000);
-console.log("tests complete..")
